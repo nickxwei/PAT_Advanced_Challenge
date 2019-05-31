2a59ea60113b47248476cb794c98ec35bf64cec2
Nick <hellonickwei@gmail.com>
2019-05-31 19:00:30 +0800

    :sparkles: feat: achieve target #5

---
+++ 1005/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    string a;
+    int sum = 0;
+    string engArr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    cin >> a;
+    for(auto x : a)
+        sum += (x - '0');
+    string b = to_string(sum);
+    cout << engArr[b[0] - '0'];
+    for(auto it = b.begin() + 1; it < b.end(); it++)
+        cout << " " + engArr[*it - '0'];
+    return 0;
+}
