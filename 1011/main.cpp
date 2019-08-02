57f755ca029da9957fa859d4069d7a08cd31a0d4
Nick <hellonickwei@gmail.com>
2019-08-02 18:14:57 +0800

    :sparkles: feat: achieve target #11

---
+++ 1011/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+float game_odd_array[3][3];
+char game_result_array[3] = {'W', 'T', 'L'};
+int max_ratio[3];
+
+int main(void)
+{
+    int i,j;
+    float max = -1.0;
+    float result = 1.0;
+    for(i = 0; i < 3; i++) {
+        max = -1.0;
+        for(j = 0; j < 3; j++) {
+            float temp;
+            cin >> temp;
+            game_odd_array[i][j] = temp;
+            if(temp >= max) {
+                max = temp;
+                max_ratio[i] = j;
+            }
+        }
+    }
+    
+    for(i = 0; i < 3; i++) {
+        cout << game_result_array[max_ratio[i]] << ' ';
+        result *= game_odd_array[i][max_ratio[i]];
+    }
+    result = (result * 0.65 - 1) * 2;
+    cout << fixed << setprecision(2) <<  result << endl;
+    return 0;
+}
