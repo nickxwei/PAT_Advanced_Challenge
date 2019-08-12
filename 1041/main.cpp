ae2dbde79cc4e43ba421e7777c94cfd2c810e3f7
Nick <hellonickwei@gmail.com>
2019-08-12 18:02:43 +0800

    :sparkles: feat: achieve target #41

---
+++ 1041/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[100001], m[100000];
+
+int main(void)
+{
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        a[i] = temp;
+
+        m[temp]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if(m[a[i]] == 1) {
+            cout << a[i] << endl;
+            return 0;
+        }
+    }
+
+    cout << "None" << endl;
+
+    return 0;
+}
